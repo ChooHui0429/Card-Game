5a59593b1f4f01e77b18aacf7405016180aab05f
ChooHui0429 <78590126+ChooHui0429@users.noreply.github.com>
2021-03-02 21:30:06 +0800

    Update Main.java

--- Src/Main.java
+++ Src/Main.java
@@ -5,40 +5,21 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
  
-public class Main extends Application
-{
-    public static void main(String[] args)
-    {
-        Application.launch(args);
+public class Main extends Application{
+
+    // Declaration of Stage and all scene in UI
+    Stage window;
+    Scene i;
+     public static void main(String[] args){
+         launch(args);// Main method to launch whole UI
     }
- 
+
     @Override
-    public void start(Stage stage)
-    {
-        // Create the Text
-        Text text = new Text("Hello JavaFX");
-        // Create the VBox
-        VBox root = new VBox();
-        // Add the Text to the VBox
-        root.getChildren().add(text);
-        // Set the Size of the VBox
-        root.setMinSize(350, 250);
- 
-        // Create the Scene
-        Scene scene = new Scene(root);
-         
-        // Set the Properties of the Stage
-        stage.setX(100);
-        stage.setY(200);
-        stage.setMinHeight(300);
-        stage.setMinWidth(400);
- 
-        // Add the scene to the Stage
-        stage.setScene(scene);
-        // Set the title of the Stage
-        stage.setTitle("Your first JavaFX Example");
-        // Display the Stage
-        stage.show();
+    public void start(Stage primaryStage){
+
+        // Declaration of main stage in UI
+        window = primaryStage;
+
     }
  
 }
