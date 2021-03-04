6eb3fd12f8867c30820025909d64d49e2ab586f2
Daedalus6S <thlim2009@hotmail.com>
2021-03-04 16:27:37 +0800

    Player class

---
+++ Src/Player.java
@@ -0,0 +1,31 @@
+package Src;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Player implements Comparable<Player> {
+    protected String name;
+    protected Queue<Hand> hands = new LinkedList<Hand>();
+
+    public Player(String n) {
+        name = n;
+    }
+
+    public Player(String n, Queue<Hand> h) {
+        name = n;
+        hands = h;
+    }
+
+    public void addHand(Hand h) {
+        hands.add(h);
+    }
+
+    public int compareTo(Player player) {
+        return name.compareTo(player.name);
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + hands.toString();
+    }
+}
