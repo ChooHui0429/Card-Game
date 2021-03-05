b21e8aa1692e2d2f89a103cb2ab8c14d07370a87
ChooHui0429 <78590126+ChooHui0429@users.noreply.github.com>
2021-03-05 22:20:08 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -513,6 +513,7 @@ public class Main extends Application{
                 }
                 scoreBoardPlayer1.setText("Player 1 =" + score1);
                 scoreBoardPlayer2.setText("Player 2 =" + score2);
+                scoreBoardPlayer3.setText("");
                 window.setScene(scoreBoard);
             }
             
